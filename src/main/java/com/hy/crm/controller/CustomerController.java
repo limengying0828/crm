ae78fea07774b9d3e57ad52c7b45ccbd8a19867c
lmy <1662585312@qq.com>
2020-09-02 16:45:17 +0800

    客户下拉框查询

--- src/main/java/com/hy/crm/controller/CustomerController.java
+++ src/main/java/com/hy/crm/controller/CustomerController.java
@@ -1,9 +1,16 @@
 package com.hy.crm.controller;
 
 
+import com.hy.crm.pojo.Customer;
+import com.hy.crm.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("//customer")
 public class CustomerController {
-
+    @Autowired
+    private ICustomerService service;
+    @GetMapping("/queryCustomer.do")
+    @ResponseBody
+    public List<Customer> queryCustomer(){
+       return service.list();
+    }
 }
