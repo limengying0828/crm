18a9fabd1e9b27a070c00379c93cf215c6559947
lmy <1662585312@qq.com>
2020-09-02 08:54:40 +0800

    添加bo和工具类

---
+++ src/main/java/com/hy/crm/bo/pml/DocumentaryBo.java
@@ -0,0 +1,9 @@
+package com.hy.crm.bo.pml;
+
+/**
+ * @Author: 李梦莹
+ * @create: 2020/9/2 8:53
+ * @Description:
+ */
+public class DocumentaryBo {
+}
---
+++ src/main/java/com/hy/crm/utils/MsgUtils.java
@@ -0,0 +1,9 @@
+package com.hy.crm.utils;
+
+/**
+ * @Author: 李梦莹
+ * @create: 2020/9/2 8:52
+ * @Description:
+ */
+public class MsgUtils {
+}
