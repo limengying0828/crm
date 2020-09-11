7db74d8b4513a01ab313fd0801eb36bf81478162
pml <2486900385@qq.com>
2020-09-11 18:07:07 +0800

    提交页面

---
+++ src/main/java/com/hy/crm/bo/pml/replyBo.java
@@ -0,0 +1,15 @@
+package com.hy.crm.bo.pml;
+
+import com.hy.crm.pojo.Invitation;
+import com.hy.crm.pojo.Reply;
+import com.hy.crm.pojo.User;
+
+/**
+ * @Author 潘梦丽
+ * @create 2020/9/11
+ */
+public class replyBo {
+    private Invitation invitation;
+    private User user;
+    private Reply reply;
+}
